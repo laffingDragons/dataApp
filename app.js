63a5a257a932322eb18bfd3be4b4c8537d865760
laffingragons <akshaypatil,vit@gmail.com>
2018-01-04 09:45:48 +0530

    best project ever

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ var bodyParser = require('body-parser');
 //calling mongoose module
 var mongoose = require('mongoose');
 
-app.use(bodyParser.json({limit: '10mb',extended: true}));
-app.use(bodyParser.urlencoded({limit: '10mb',extended: true}));
+app.use(bodyParser.json({
+    limit: '10mb',
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: true
+}));
 
 var dbPath = "mongodb://localhost/myblogapp";
 //command to connect with database
 db = mongoose.connect(dbPath);
 
 mongoose.connection.once('open', function () {
-    
+
     console.log("Database Connection open successfully");
 });
 
@@ -94,11 +100,13 @@ app.post('/blog/create', function (req, res) {
 
 //start route to edit a blog using _id
 
-app.put('/blogs/:id/edit', function(req, res) {
+app.put('/blogs/:id/edit', function (req, res) {
 
     var update = req.body;
 
-    blogModel.findOneAndUpdate({'_id': req.params.id}, update, function (err, result) {
+    blogModel.findOneAndUpdate({
+        '_id': req.params.id
+    }, update, function (err, result) {
 
         if (err) {
             console.log("some Error");
@@ -110,20 +118,35 @@ app.put('/blogs/:id/edit', function(req, res) {
 });
 
 //start the route to delete a blog
-app.post('/blogs/:id/delete',function(req, res){
-    
-    blogModel.remove({'_id':req.params.id},function(err, result){
-        if(err){
-           console.log("some Error");
-           res.send(err);
-       }
-       else{
-           res.send(result);
-       }
-    });//end user model find
+app.post('/blogs/:id/delete', function (req, res) {
+
+    blogModel.remove({
+        '_id': req.params.id
+    }, function (err, result) {
+        if (err) {
+            console.log("some Error");
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    }); //end user model find
 });
 
-//
-app.listen(3000, function(){
+//starting with error handling
+app.get('*', function (req, res, next) {
+    res.status = 404;
+    next('Path not Found');
+}); //end of normal routes
+//error handling middleware
+app.use(function (err, req, res, next) {
+    console.log('Error handler was in use');
+    if (res.status == 404) {
+        res.send('Hacking can result in ban');
+    } else {
+        res.send(err);
+    }
+});
+//start listing on port 3000
+app.listen(3000, function () {
     console.log('Listening on Port 3000');
 });
